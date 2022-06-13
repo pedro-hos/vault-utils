622a98ce3dce68388eaeee07d2573a8306fbdc26
Pedro Hos <pedro-hos@outlook.com>
2022-06-13 18:43:13 -0300

    just few improvements

--- vault-utils/src/main/java/io/quarkiverse/filevault/MainCommand.java
+++ vault-utils/src/main/java/io/quarkiverse/filevault/MainCommand.java
@@ -13,19 +13,23 @@ import picocli.CommandLine.Option;
 @Command(name = "Encrypt Secret Util", mixinStandardHelpOptions = true)
 public class MainCommand implements Runnable, QuarkusApplication {
 
+    private static final String DEFAULT_SECRET_KEY = "somearbitrarycrazystringthatdoesnotmatter";
+    private static final String DEFAULT_ITERATION_CODE = "65536";
+    private static final String DEFAULT_SALT = "1234abcd";
+
     @Inject
     CommandLine.IFactory factory; 
     
-    @Option(names = {"-s", "--salt"}, defaultValue = "1234abcd", description = "8 character salt")
+    @Option(names = {"-s", "--salt"}, defaultValue = DEFAULT_SALT, description = "(optional) 8 character salt")
     String salt;
     
-    @Option(names = {"-i", "--iteration"}, defaultValue = "65536", description = "Iteration count")
+    @Option(names = {"-i", "--iteration"}, defaultValue = DEFAULT_ITERATION_CODE, description = "(optional) Iteration count")
     int iterationCount;
     
-    @Option(names = {"-sk", "--secrect-key"}, defaultValue = "somearbitrarycrazystringthatdoesnotmatter", description = "Secret Key")
+    @Option(names = {"-sk", "--secrect-key"}, defaultValue = DEFAULT_SECRET_KEY, description = "(optional) Secret Key")
     String secretKey;
     
-    @Option(names = {"-p", "--keystore-password"}, description = "Keystore password", required = true)
+    @Option(names = {"-p", "--keystore-password"}, description = "(mandatory) Keystore password", required = true)
     String keystorePassword;
 
     @Override
@@ -37,15 +41,15 @@ public class MainCommand implements Runnable, QuarkusApplication {
         System.out.println("Please add the following paramenters on your application.properties file, and replace the <key> value!");
         System.out.println("quarkus.file.vault.provider.<key>.encrypted=true");
         
-        if(!"1234abcd".equals(salt)) {
+        if(!DEFAULT_SALT.equals(salt)) {
             System.out.println("quarkus.file.vault.provider.<key>.salt=" + salt);
         }
         
-        if(!"somearbitrarycrazystringthatdoesnotmatter".equals(secretKey)) {
+        if(!DEFAULT_SECRET_KEY.equals(secretKey)) {
             System.out.println("quarkus.file.vault.provider.<key>.secretKey=" + secretKey);
         }
         
-        if(65536 != iterationCount) {
+        if(Integer.parseInt(DEFAULT_ITERATION_CODE) != iterationCount) {
             System.out.println("quarkus.file.vault.provider.<key>.iteration-count=" + iterationCount);
         }
         
